13d1107175c12534d658080b471c90c0d165d214
Azat <ismagilov.code@gmail.com>
2019-11-12 17:08:53 +0300

    Search for participants

--- vkoshp/admin.py
+++ vkoshp/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Contest, Team, Participant, HandleInvoice
 
 # Register your models here.
+class ParticipantAdmin(UserAdmin):
+	model = Participant
+	list_display = ('team', 'name', 'medal', 'codeforces_handle')
+	search_fields = ('name','codeforces_handle',)
+	readonly_fields = ()
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+	ordering = ()
 
 admin.site.register(Contest)
 admin.site.register(Team)
-admin.site.register(Participant)
+admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(HandleInvoice)
